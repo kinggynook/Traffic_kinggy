9097a59980d8d2d3da7784b87f5d5efa6c530dec
Kinggy <kinggy_hoo@hotmail.com>
2016-07-13 11:50:04 +0700

    Traffic_kinggy

---
+++ app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/traffic_kinggy/MainActivity.java
@@ -0,0 +1,35 @@
+package pnu.ntc.b.appapinya.apinyaburisri.traffic_kinggy;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createListView();
+
+    } //Main Method
+
+    private void createListView() {
+        MyData objMyData = new MyData();
+        int[] intIcon = objMyData.icon();
+        String[] strTitle = objMyData.title();
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intIcon, strTitle);
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(objMyAdapter);
+
+    } //createListView
+
+    public void clickSound(View view) {
+        Intent objIntent = new Intent(Intent.ACTION_VIEW);
+        objIntent.setData(Uri.parse("http://ntc.pnu.ac.th"));
+        startActivity(objIntent);
+    } //click_here
+}
---
+++ app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/traffic_kinggy/MyAdapter.java
@@ -0,0 +1,53 @@
+package pnu.ntc.b.appapinya.apinyaburisri.traffic_kinggy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by apinyaburisri on 6/22/2016 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+    private Context objContext;
+    private int[] trafficInts;
+    private String[] trafficStrings;
+
+    public MyAdapter(Context objContext, int[] trafficInts, String[] trafficStrings) {
+        this.objContext = objContext;
+        this.trafficInts = trafficInts;
+        this.trafficStrings = trafficStrings;
+    } //Constructor
+
+    @Override
+    public int getCount() {
+        return trafficStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+        //Show Icon
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imvShowIcon);
+        iconImageView.setBackgroundResource(trafficInts[i]);
+        //Show Title
+        TextView titleTextView = (TextView) view1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(trafficStrings[i]);
+
+        return view1;
+    }
+}//Main Class
