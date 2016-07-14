65ca2d8d25781f4372d38cb99bd6740a2dc594f0
ApinyaKINGGY <kinggy_hoo@hotmail.com>
2016-07-14 16:23:37 +0700

    Create XML Detail

---
+++ app/src/main/java/pnu/ntc/b/appapinya/apinyaburisri/traffic_kinggy/ShowDetailActivity.java
@@ -0,0 +1,13 @@
+package pnu.ntc.b.appapinya.apinyaburisri.traffic_kinggy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+    }
+}
